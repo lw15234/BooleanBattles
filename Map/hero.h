c32c6ec6cdf944be89aa3a00e650c6779140ed26
J0sh1337 <mrjoshuaali@yahoo.co.uk>
2015-11-24 16:03:21 +0000

    hero.h

---
+++ Map/hero.h
@@ -0,0 +1,31 @@
+#ifndef _HERO_H_INCLUDED
+#define _HERO_H_INCLUDED
+
+#include "SDL2/include/SDL.h"
+#include "util.h"
+
+
+typedef struct Hero Hero;
+
+/*Represents Hero
+ * pos - position of hero in world coordinates
+ * displayRect - size and position of hero on screen
+ * textureRect - rect which contains hero image on texture
+ * texture - texture containing hero image
+ */
+
+struct Hero {
+	Rect worldRect;
+	SDL_Rect displayRect;
+	SDL_Rect textureRect;
+	SDL_Texture* texture;
+};
+
+void HeroInit(Hero* hero, Rect worldRect, SDL_Rect textureRect, SDL_Texture* texture);
+void HeroRender(const Hero* hero, SDL_Renderer* renderer);
+void HeroMoveLeft(Hero* hero);
+void HeroMoveRight(Hero* hero);
+void HeroMoveUp(Hero* hero);
+void HeroMoveDown(Hero* hero);
+
+#endif
