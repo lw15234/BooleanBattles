6277e0578210cae8f1242dda8734bb5593e25caf
lw15234 <lw15234@my.bristol.ac.uk>
2015-12-02 14:54:36 +0000

    Test bmp format

--- Logic/attackAnimation.c
+++ Logic/attackAnimation.c
@@ -3,6 +3,8 @@
 #include <math.h>
 
 #define FRAMES 10
+#define WIDTH 250
+#define HEIGHT 250
 
 typedef struct projectile{
     SDL_Texture *tex;
@@ -11,13 +13,6 @@ typedef struct projectile{
     SDL_Rect spriteClips[FRAMES];
 }projectile;
 
-typedef struct display{
-    int width, height;
-    SDL_Window *win;
-    SDL_Surface *sur;
-    SDL_Renderer *ren;
-    SDL_Event *e;
-}display;
 
 projectile *createProjectile();
 void destroyProjectile(projectile *p);
@@ -25,7 +20,7 @@ void renderAttack(int attack, projectile *p, SDL_Renderer *ren);
 void selectAttack(int attack, projectile *p);
 void setAttackAttr(projectile *p, SDL_Renderer *ren);
 void fillClips(projectile *p);
-void animateAttack(projectile *p, SDL_Renderer *ren, int width, int height);
+void animateAttack(projectile *p, SDL_Renderer *ren);
 
 
 projectile *createProjectile(){
@@ -68,10 +63,9 @@ void attackManager(int attack, SDL_Renderer *ren)
 
 void renderAttack(int attack, projectile *p, SDL_Renderer *ren)
 {
-    int height = 250, width = 250;
     selectAttack(attack, p);
-    setAttackAttr(p, ren);
-    animateAttack(p, ren, width, height);
+    p->tex = SDL_CreateTextureFromSurface(ren, p->sur);
+    animateAttack(p, ren);
 }
 
 void selectAttack(int attack, projectile *p)
@@ -79,25 +73,36 @@ void selectAttack(int attack, projectile *p)
 	switch(attack){
         case 0:
             p->sur = SDL_LoadBMP("projectiles/fire.bmp");
+            if(!p->sur){
+                printf("Could not load file: %s\n", SDL_GetError());
+                exit(1);
+            }
             break;
         case 1:
             p->sur = SDL_LoadBMP("projectiles/ice.bmp");
+            if(!p->sur){
+                printf("Could not load file: %s\n", SDL_GetError());
+                exit(1);
+            }
             break;
         case 2:
             p->sur = SDL_LoadBMP("projectiles/lightning.bmp");
+            if(!p->sur){
+                printf("Could not load file: %s\n", SDL_GetError());
+                exit(1);
+            }
             break;
         case 3:
             p->sur = SDL_LoadBMP("projectiles/normal.bmp");
+            if(!p->sur){
+                printf("Could not load file: %s\n", SDL_GetError());
+                exit(1);
+            }
             break;
     }
 }
 
-void setAttackAttr(projectile *p, SDL_Renderer *ren)
-{
-    p->nextTex = SDL_CreateTextureFromSurface(ren, p->sur);
-    SDL_SetRenderDrawColor(ren, 255, 255, 255, 0);
-    p->tex = p->nextTex;
-}
+
 
 void fillClips(projectile *p)
 {
@@ -110,22 +115,19 @@ void fillClips(projectile *p)
     }
 }
 
-void animateAttack(projectile *p, SDL_Renderer *ren, int width, int height)
+void animateAttack(projectile *p, SDL_Renderer *ren)
 {
     int i;
-    SDL_Rect *clip = (SDL_Rect *)malloc(sizeof(SDL_Rect));
-    SDL_Rect renderQuad = {0, 0, width, height};
+    SDL_Rect *clip = NULL;
+    SDL_Rect renderQuad = {0, 0, WIDTH, HEIGHT};
     Uint32 t;
 
-    for(i = 0; i < 500; i++){
+    for(i = 0; i < 100; i++){
         clip = &p->spriteClips[i % FRAMES];
         renderQuad.x = i * 10;
-        renderQuad.w = clip->w;
-        renderQuad.h = clip->h;
         SDL_RenderCopy(ren, p->tex, clip, &renderQuad);
         SDL_RenderPresent(ren);
         t = SDL_GetTicks();
         SDL_Delay(20 - (t % 20));
     }
-    free(clip);
 }
